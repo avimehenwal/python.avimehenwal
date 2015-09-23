cb1fa3f922c7b81ae5eeb3c2fbe5969ee3a10ddc
avimehenwal <avi.mehanwal@gmail.com>
2015-09-23 16:07:22 +0530

    url resolver unittest

--- TDD/superlists/lists/tests.py
+++ TDD/superlists/lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
+        
 # Functional TC  -> from the point of view of user
 # Unit Test Case -> from the point of view of developer
+
 
 class SmokeTest(TestCase):
 
@@ -9,3 +13,9 @@ class SmokeTest(TestCase):
         self.assertEqual(1+1, 3)
 
 
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        print(found)
+        self.assertEqual(found.func, home_page)
