25f48eb4348595b4e86643c329988434c21c8669
avimehenwal <avi.mehanwal@gmail.com>
2015-09-23 16:43:51 +0530

    tdd HttpRequest from django

--- TDD/superlists/lists/tests.py
+++ TDD/superlists/lists/tests.py
@@ -1,16 +1,17 @@
-from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
         
 # Functional TC  -> from the point of view of user
 # Unit Test Case -> from the point of view of developer
 
 
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+#class SmokeTest(TestCase):
+#
+#    def test_bad_maths(self):
+#        self.assertEqual(1+1, 3)
 
 
 class HomePageTest(TestCase):
@@ -19,3 +20,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         print(found)
         self.assertEqual(found.func, home_page)
+
+    def test_homepage_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
