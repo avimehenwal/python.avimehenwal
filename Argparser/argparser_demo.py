b1affe0af501602e8661674e18268590b84ebec5
avimehenwal <avi.mehanwal@gmail.com>
2016-01-12 23:19:59 +0530

    :more arguments added

--- Argparser/argparser_demo.py
+++ Argparser/argparser_demo.py
@@ -14,6 +14,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-v", help="verbose mode")
+parser.add_argument("-a", help="a mode")
+parser.add_argument("-b", help="b verbose mode")
+parser.add_argument("-c", help="c verbose mode")
+parser.add_argument("-d", help="d verbose mode")
+parser.add_argument("-e", help="e verbose mode")
 
 args = parser.parse_args()
 print(args)
+print(type(args))
+print(args.a)
+
