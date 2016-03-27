99c22cc8ad2492e91bf0dae03a6e23862767bd9a
avimehenwal <avi.mehanwal@gmail.com>
2016-03-27 13:54:34 +0530

    python for..else and try..else..finally

---
+++ hidden_features.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+FileName   hidden_features.py 
+Author     avimehenwal
+Created    Sun Mar 27 09:52:24 IST 2016
+
+some cool and interesting stuff with python
+
+
+try:
+    # try to do something
+except:
+    # handle errors
+else:
+    # only executed if no errors occurred
+finally:
+    # always executed 
+"""
+
+# For ... Else loop
+def for_else(n):
+    for i in n:
+        print 'Loop ', i
+    else:
+        print 'Else ', n
+        
+if __name__ == '__main__':
+    for_else('avi')
+    for_else([])
+    for_else([1,3,4])
+
+# END
